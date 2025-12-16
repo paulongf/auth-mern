1a4ee5ae04e4c2323b59dbfd6993722906f85597
Paulongf <paulongf@hotmail.com>
2025-12-16 11:05:50 +0000

    Trying to debug

--- server/server.js
+++ server/server.js
@@ -11,16 +11,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || origin === process.env.CLIENT_URL) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
